f999458a8f23caa955b23ac67edcc34fa2bc0acf
Shrishail Teli <shrishailmteli@gmail.com>
2022-06-26 14:50:12 +0530

    Update ExpenseManagerSystemTest.java

--- src/test/java/com/expense/tests/ExpenseManagerSystemTest.java
+++ src/test/java/com/expense/tests/ExpenseManagerSystemTest.java
@@ -62,8 +62,8 @@ public class ExpenseManagerSystemTest {
 			//You need to enter your JIRA project key in below line
         	Issue issueName = jira.createIssue("", "Bug").field(Field.SUMMARY, result.getMethod().getMethodName() +"is failed due to: "+ result.getThrowable().toString()).field(Field.DESCRIPTION, "get the description").execute();
         	System.out.println("Issue is created in Jira with Issue Key: "+issueName.getKey());
-	*/
-        }
+	
+        } */
 
 	}
 	
